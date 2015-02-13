b48ae90d0dd52cb10fecfc67b32c507c4e34896d
EyuelAbebe <eyueld@gmail.com>
2015-02-12 19:32:45 -0500

    Improved each methods query string.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,10 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +17,15 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+
+    DB = psycopg2.connect("dbname=forum")
+    cur = DB.cursor()
+    query = "SELECT * from posts"
+    cur.execute(query)
+    all_posts = cur.fetchall()
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in all_posts]
     posts.sort(key=lambda row: row['time'], reverse=True)
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +35,14 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+
+    DB = psycopg2.connect("dbname=forum")
+    cur = DB.cursor()
+    query = "INSERT INTO posts (content) values ('%s')"%(content,)
+    cur.execute(query)
+    DB.commit()
+    DB.close()
+
+  
+
+
