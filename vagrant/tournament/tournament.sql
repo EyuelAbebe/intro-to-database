7ffd3585ec8cfd4ec39c767c12a5791a5801f1e4
EyuelAbebe <eyueld@gmail.com>
2015-03-06 19:22:20 -0500

    Created two tables to hold players and matches.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,11 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Table for player names
+CREATE TABLE players ( id SERIAL NOT NULL UNIQUE  PRIMARY KEY,
+                       full_name VARCHAR(90) );
 
+-- Table for all matches.
+CREATE TABLE matches ( id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                       winner integer references players (id),
+                       loser integer references players (id) );
